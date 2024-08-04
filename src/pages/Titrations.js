57fae16aaa827b2e51018d96ffe6c9ee9105e1d8
ChetanGorantla <chetangorantla7@gmail.com>
2024-08-04 10:18:49 -0500

    fixing pH

--- src/pages/Titrations.js
+++ src/pages/Titrations.js
@@ -115,8 +115,8 @@ const Titrations = () => {
 
 
   const renderPHForm = () => {
-    const isAcidWithBaseTitration = selectedOption.includes('acid with strong base');
-    const isBaseWithAcidTitration = selectedOption.includes('base with strong acid');
+    const isAcidWithBaseTitration = selectedOption.includes('base with strong acid');
+    const isBaseWithAcidTitration = selectedOption.includes('');
     const isStrongAcidBaseTitration = selectedOption.includes('titrating strong');
   
     return (
