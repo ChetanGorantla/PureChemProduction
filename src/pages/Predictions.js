4264c1cd0aa3b0de67c7e357036592342fb0f5a0
ChetanGorantla <chetangorantla7@gmail.com>
2024-08-06 08:30:14 -0500

    hiding api

--- src/pages/Predictions.js
+++ src/pages/Predictions.js
@@ -26,12 +26,12 @@ const Predictions = () => {
     try {
       let response;
       if (type === 'melting') {
-        response = await axios.post('https://purechem-263a4a4b5c6d.herokuapp.com/sort-melting', {
+        response = await axios.post('{process.env.BACKEND_API}/sort-melting', {
           molecules: meltingString,
         });
         setResponseDataMelting(response.data.result);
       } else if (type === 'solubility') {
-        response = await axios.post('https://purechem-263a4a4b5c6d.herokuapp.com/sort-solubility', {
+        response = await axios.post('{process.env.BACKEND_API}/sort-solubility', {
           compounds: solubilityString,
         });
         setResponseDataSolubility(response.data.result);
